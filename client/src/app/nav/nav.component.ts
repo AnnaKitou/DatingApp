8894c0f55fd5c0668232a290795da39f34fd4838
kitouanna@gmail.com <kitouanna@gmail.com>
2022-08-23 22:40:03 +0300

    Getting current user Pipe

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -7,37 +9,25 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-model:any={}
-loggedIn:boolean
-  constructor(private accountService:AccountService) { }
+  model: any = {}
+  loggedIn: boolean
+
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
   }
 
-  login(){
-   this.accountService.login(this.model).subscribe(response=>
-    {
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn=true
-    }, error=>{
+    }, error => {
       console.log(error)
     }
     );
-    
   }
 
-  logout(){
-    this.accountService.logout();
-    this.loggedIn=false;
+  logout() {
+    this.loggedIn = false;
   }
 
-  getCurrentUser(){
-    this.accountService.currentUser$.subscribe(user=>{
-      this.loggedIn=!!user;
-    },error=>
-    {
-      console.log(error);
-    })
-  }
 }
