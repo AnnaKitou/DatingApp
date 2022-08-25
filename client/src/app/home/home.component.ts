554e4b7bc47ca40e6b38f18913669caf0cf2988e
kitouanna@gmail.com <kitouanna@gmail.com>
2022-08-25 23:06:08 +0300

    Parsing data from child to parent

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -22,4 +22,7 @@ export class HomeComponent implements OnInit {
   getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe(users => this.users = users);
   }
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +6,8 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-@Input() usersFromHomeComponent:any;
+  @Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
   constructor() { }
@@ -19,6 +20,6 @@ export class RegisterComponent implements OnInit {
   }
 
   cancel() {
-    console.log('cancelled');
+    this.cancelRegister.emit(false);
   }
 }
