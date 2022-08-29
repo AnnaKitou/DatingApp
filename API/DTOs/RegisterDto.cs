9a31d2c1182568829b043e21e69f2245da2bbaac
kitouanna@gmail.com <kitouanna@gmail.com>
2022-08-29 19:40:29 +0300

    Error Handling

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -11,6 +11,7 @@ namespace API.DTOs
         [Required]
         public string UserName { get; set; }
         [Required]
+        [StringLength(8,MinimumLength =4)]
         public string Password { get; set; }
     }
 }
