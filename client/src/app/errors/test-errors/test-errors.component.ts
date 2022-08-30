aaeba2d6d8fce06e48564e8ad58329c76f6a7294
kitouanna@gmail.com <kitouanna@gmail.com>
2022-08-30 19:57:18 +0300

    ErrorHandling

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestErrorsComponent implements OnInit {
   baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[]=[];
 
   constructor(private http: HttpClient) { }
 
@@ -49,10 +50,11 @@ export class TestErrorsComponent implements OnInit {
   }
 
   get400ValidationError() {
-    this.http.post(this.baseUrl + 'account/register',{}).subscribe(response => {
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe(response => {
       console.log(response);
     }, error => {
       console.log(error);
+      this.validationErrors=error;
     })
   }
 
