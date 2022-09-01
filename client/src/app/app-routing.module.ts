0855bc7627b890be8f54a3b2b6c66373709d5f60
kitouanna@gmail.com <kitouanna@gmail.com>
2022-09-01 19:59:40 +0300

    Adding Data Through Json Generator

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -13,20 +13,20 @@ import { AuthGuard } from './_guards/auth.guard';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
-    path:'',
-    runGuardsAndResolvers:'always',
-    canActivate:[AuthGuard],
-    children:[
-      { path: 'members', component: MemberListComponent, canActivate:[AuthGuard] },
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
       { path: 'members/:id', component: MemberDetailComponent },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ]
   },
-  {path:'errors',component:TestErrorsComponent},
-  {path:'not-found',component:NotFoundComponent},
-  {path:'server-error',component:ServerErrorComponent},
-  { path: '**', component: NotFoundComponent,pathMatch:'full'},
+  { path: 'errors', component: TestErrorsComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: '**', component: NotFoundComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
