7183e81930e7696ec02a310acff52f3d8dd594dd
kitouanna@gmail.com <kitouanna@gmail.com>
2022-09-05 19:58:26 +0300

    Automapper

--- API/DTOs/MemberDTO.cs
+++ API/DTOs/MemberDTO.cs
@@ -20,7 +20,7 @@ namespace API.DTOs
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<Photo> Photos { get; set; }
+        public ICollection<PhotoDTO> Photos { get; set; }
       
     }
 }
