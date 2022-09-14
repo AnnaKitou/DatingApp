e45b73ac7b8d1f9a4e026985b9c1ee5edbca066e
kitouanna@gmail.com <kitouanna@gmail.com>
2022-09-14 22:14:19 +0300

    Edit Profile

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -26,7 +26,7 @@ export class MemberEditComponent implements OnInit {
   }
 
   loadMember() {
-    this.memberService.getMember(this.user.username).subscribe(member => {
+    this.memberService.getMember(this.user.userName).subscribe(member => {
       this.member = member;
     })
   }
