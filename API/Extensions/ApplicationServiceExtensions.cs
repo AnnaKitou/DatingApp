0ed22700e8e2313f996451e560dc134091d77fc1
kitouanna@gmail.com <kitouanna@gmail.com>
2022-09-19 20:16:27 +0300

    Cloudinary - Photo Management

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -12,10 +12,13 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Extensions
 {
     public static class ApplicationServiceExtensions
-    { public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
@@ -24,7 +27,7 @@ namespace API.Extensions
             });
             return services;
         }
-        
-        
+
+
     }
 }
