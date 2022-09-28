44e7127ac13c169f3f51ae86c3a7da3faefd86f8
kitouanna@gmail.com <kitouanna@gmail.com>
2022-09-28 19:03:03 +0300

    Register Form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { TextInputComponent } from './_forms/text-input/text-input.component';
+import { DateInputComponent } from './_forms/date-input/date-input.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { TextInputComponent } from './_forms/text-input/text-input.component';
     MemberCardComponent,
     MemberEditComponent,
     PhotoEditorComponent,
-    TextInputComponent
+    TextInputComponent,
+    DateInputComponent
   ],
   imports: [
     BrowserModule,
