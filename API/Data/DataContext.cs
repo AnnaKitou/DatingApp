70babfbb42045b565aa90506a811b20ceedf24ea
kitouanna@gmail.com <kitouanna@gmail.com>
2022-10-12 23:30:33 +0300

    Adding Likes Service

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -12,7 +12,28 @@ namespace API.Data
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-        
-        public DbSet<AppUser> Users{get; set;}
+
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+            .HasKey(k => new { k.SourceUserId, k.LikedUserId });
+
+            builder.Entity<UserLike>()
+            .HasOne(s => s.SourceUser)
+            .WithMany(l => l.LikeUsers)
+            .HasForeignKey(s => s.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+            .HasOne(s => s.LikedUser)
+            .WithMany(l => l.LikedByUsers)
+            .HasForeignKey(s => s.LikedUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -19,6 +19,7 @@ namespace API.Extensions
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
             services.AddScoped<LogUserActivity>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
