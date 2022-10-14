cf13bac81ef04f24edbbe21aa2e515268431599b
kitouanna@gmail.com <kitouanna@gmail.com>
2022-10-14 19:21:29 +0300

    Likes

--- API/Controllers/LikesController.cs
+++ API/Controllers/LikesController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
 using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -26,8 +28,34 @@ namespace API.Controllers
         public async Task<ActionResult> AddLike(string username)
         {
             var sourceUserId = User.GetUserId();
-            var likedUser=await _userRepository.GetUserByUsernameAsync(username);
-            var sourceUser=await _likesRepository.GetUserWithLikes(sourceUserId);
+            var likedUser = await _userRepository.GetUserByUsernameAsync(username);
+            var sourceUser = await _likesRepository.GetUserWithLikes(sourceUserId);
+
+            if (likedUser == null) return NotFound();
+
+            if (sourceUser.UserName == username) return BadRequest("You cannot like Yourself");
+
+            var userLike = await _likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+
+            if (userLike != null) return BadRequest("You already like this user");
+            userLike = new UserLike
+            {
+                SourceUserId = sourceUserId,
+                LikedUserId = likedUser.Id
+            };
+            sourceUser.LikeUsers.Add(userLike);
+
+            if (await _userRepository.SaveAllASync()) return Ok();
+
+            return BadRequest("Failed to like User");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LikeDTO>>> GetUserLikes(string predicate)
+        {
+            var users = await _likesRepository.GetUserLikes(predicate, User.GetUserId());
+
+            return Ok(users);
         }
     }
 }
