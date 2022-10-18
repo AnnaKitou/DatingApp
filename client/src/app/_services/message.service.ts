d5c7180ae05031ac86001794c39b33c5c70aead5
kitouanna@gmail.com <kitouanna@gmail.com>
2022-10-18 19:54:11 +0300

    Messages Client Side

---
+++ client/src/app/_services/message.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Message } from '../_models/message';
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getMessages(pageNumber, pageSIze, container) {
+    let params = getPaginationHeaders(pageNumber, pageSIze);
+    params = params.append('Container', container);
+    return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http);
+  }
+}
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,4 +1,8 @@
+import { Container } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import { MessageService } from '../_services/message.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[];
+  pagination: Pagination;
+  container = 'Unread';
+  pageNumber = 1;
+  pageSize = 5;
 
-  constructor() { }
+
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.loadMessages();
   }
 
+  loadMessages() {
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(response => {
+      this.messages = response.result;
+      this.pagination = response.pagination;
+    })
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
