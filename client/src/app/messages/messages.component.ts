eb3032a8f3ae42029b5c63dc7f2e9773fb2e0cbb
kitouanna@gmail.com <kitouanna@gmail.com>
2022-10-22 20:47:51 +0300

    Messages

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -10,12 +10,12 @@ import { MessageService } from '../_services/message.service';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
-  messages: Message[]=[];
+  messages: Message[] = [];
   pagination: Pagination;
   container = 'Unread';
   pageNumber = 1;
   pageSize = 5;
-  loading=false;
+  loading = false;
 
 
   constructor(private messageService: MessageService) { }
@@ -25,11 +25,17 @@ export class MessagesComponent implements OnInit {
   }
 
   loadMessages() {
-    this.loading=true;
+    this.loading = true;
     this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(response => {
       this.messages = response.result;
       this.pagination = response.pagination;
-      this.loading=false;
+      this.loading = false;
+    })
+  }
+
+  deleteMessage(id: number) {
+    this.messageService.deleteMessage(id).subscribe(() => {
+      this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
     })
   }
 
