fdb6351cc791cb387ee7855dc9ad0567f12fcd5f
kitouanna@gmail.com <kitouanna@gmail.com>
2022-10-27 19:22:04 +0300

    Adding Admin AUth

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -11,7 +11,7 @@ import { User } from '../_models/user';
 export class AccountService {
   baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
-  currentUser$=this.currentUserSource.asObservable();
+  currentUser$ = this.currentUserSource.asObservable();
   constructor(private http: HttpClient) { }
 
   login(model: any) {
@@ -19,30 +19,38 @@ export class AccountService {
     return this.http.post(this.baseUrl + 'account/Login', model).pipe(
       map((response: User) => {
         const user = response;
-        if (user) {    
-           this.setCurrentUser(user);
-        }
-      })
-    )
-  }
-
-  register(model:any){
-    return this.http.post(this.baseUrl + 'account/register',model).pipe(
-      map((user:User)=>{
-        if(user){
+        if (user) {
           this.setCurrentUser(user);
         }
       })
     )
   }
 
-setCurrentUser(user:User){
-  localStorage.setItem('user', JSON.stringify(user));
-  this.currentUserSource.next(user);
-}
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
+
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUserSource.next(user);
+  }
 
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
