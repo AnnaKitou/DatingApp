ffae3f40db0a2c55cf2dc550b8150028cb37d276
kitouanna@gmail.com <kitouanna@gmail.com>
2022-11-02 23:58:44 +0200

    .

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject } from 'rxjs';
+
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
