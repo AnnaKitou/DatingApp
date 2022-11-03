3fe2fe01a2c1cac5f60a7ccb413c52f6a716828b
kitouanna@gmail.com <kitouanna@gmail.com>
2022-11-03 18:42:17 +0200

    Receiving Messages

--- API/Startup.cs
+++ API/Startup.cs
@@ -73,8 +73,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
-
-
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
